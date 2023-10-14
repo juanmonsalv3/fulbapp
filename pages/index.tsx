bbdf6a1c8fe2aecb411fb2d16c80c8a4a33f894f
Juan Monsalve <juanchomonsalve92@gmail.com>
2023-10-13 22:55:44 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <Button variant='contained'>Hello world</Button>
+    </div>
+  );
+}
