6c692b43d2361ee52ed9f20d19d750f701e1144b
Juan Monsalve <juanchomonsalve92@gmail.com>
2023-10-20 21:32:53 -0500

    remove trash

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,60 +1,6 @@
 import { Box, CssBaseline, Hidden, createTheme } from '@mui/material';
-import BottomNav from './BottomNav';
-import MenuAppBar from './MenuAppBar';
 import { useState } from 'react';
-import { ThemeProvider } from '@emotion/react';
 import Navigator from './Navigator';
-
-let theme = createTheme({
-  palette: {
-    primary: {
-      light: '#63ccff',
-      main: '#009be5',
-      dark: '#006db3',
-    },
-  },
-  typography: {
-    h5: {
-      fontWeight: 500,
-      fontSize: 26,
-      letterSpacing: 0.5,
-    },
-  },
-  mixins: {
-    toolbar: {
-      minHeight: 48,
-    },
-  },
-});
-
-const drawerWidth = 256;
-
-const styles = {
-  root: {
-    display: 'flex',
-    minHeight: '100vh',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  app: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  main: {
-    flex: 1,
-    padding: theme.spacing(6, 4),
-    background: '#eaeff1',
-  },
-  footer: {
-    padding: theme.spacing(2),
-    background: '#eaeff1',
-  },
-};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -64,28 +10,25 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <main>
-        <CssBaseline />
-        <nav>
-          <Hidden smUp implementation='js'>
-            <Navigator
-              PaperProps={{ style: { width: drawerWidth } }}
-              variant='temporary'
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-            />
-          </Hidden>
-          <Hidden xsDown implementation='css'>
-            <Navigator PaperProps={{ style: { width: drawerWidth } }} />
-          </Hidden>
-        </nav>
-        <Box sx={{ pb: 7 }}>
-          {/* <MenuAppBar /> */}
-          {children}
-          {/* <BottomNav /> */}
-        </Box>
-      </main>
-    </ThemeProvider>
+    <main>
+      <CssBaseline />
+      <nav>
+        <Hidden smUp implementation='js'>
+          <Navigator
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+          />
+        </Hidden>
+        <Hidden xsDown implementation='css'>
+          <Navigator />
+        </Hidden>
+      </nav>
+      <Box sx={{ pb: 7 }}>
+        {/* <MenuAppBar /> */}
+        {children}
+        {/* <BottomNav /> */}
+      </Box>
+    </main>
   );
 }
