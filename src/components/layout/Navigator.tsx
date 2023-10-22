3ce25b82edd3d99850827b7f00143da7a81d7961
Juan Monsalve <juanchomonsalve92@gmail.com>
2023-10-22 14:30:14 -0500

    idk

--- src/components/layout/Navigator.tsx
+++ src/components/layout/Navigator.tsx
@@ -13,10 +13,19 @@ import Insights from '@mui/icons-material/Insights';
 import Scoreboard from '@mui/icons-material/Scoreboard';
 import GroupIcon from '@mui/icons-material/Group';
 import StadiumIcon from '@mui/icons-material/Stadium';
+import { useRouter } from 'next/navigation';
 
-function Item({ label, icon }: { label: string; icon: ReactNode }) {
+interface ItemProps {
+  label: string;
+  target: string;
+  icon: ReactNode;
+}
+
+function Item({ label, icon, target }: ItemProps) {
+  const router = useRouter();
+
   return (
-    <ListItem key={label} disablePadding>
+    <ListItem key={label} disablePadding onClick={() => router.push(target)}>
       <ListItemButton>
         <ListItemIcon>{icon}</ListItemIcon>
         <ListItemText primary={label} />
@@ -31,10 +40,10 @@ function Navigator() {
       <Toolbar />
       <Divider />
       <List>
-        <Item label='Estadísticas' icon={<Insights />} />
-        <Item label='Partidos' icon={<Scoreboard />} />
-        <Item label='Jugadores' icon={<GroupIcon />} />
-        <Item label='Canchas' icon={<StadiumIcon />} />
+        <Item label='Estadísticas' target='/stats' icon={<Insights />} />
+        <Item label='Partidos' target='/matches' icon={<Scoreboard />} />
+        <Item label='Jugadores' target='/players' icon={<GroupIcon />} />
+        <Item label='Canchas' target='/fields' icon={<StadiumIcon />} />
       </List>
     </div>
   );
